1e067aaabb46fac87db453e75630314adbffe1e2
Bohdan <by.muha@gmail.com>
2021-04-05 19:51:02 +0300

    styling started

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -2,10 +2,10 @@
 
 const apiKey = "9d04517f3c50b24e9da372b682d112d7";
 
-const fetchCoordinates = async (city = "London") => {
+const fetchCoordinates = async (city = "Ivano-Frankivsk") => {
   try {
     const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city},uk&APPID=${apiKey}`
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`
     );
     const json = await response.json();
     return json.coord;
@@ -15,8 +15,8 @@ const fetchCoordinates = async (city = "London") => {
 };
 
 const fetchWeather = async (
-  latitude = 51.5085,
-  longitude = -0.1257,
+  latitude = 48.9215,
+  longitude = 24.7097,
   units = "metric"
 ) => {
   try {
--- src/js/index.js
+++ src/js/index.js
@@ -5,4 +5,15 @@ const initApp = () => {
   renderWeather(fetchWeather());
 };
 
-initApp();
+const submitCityButton = document.getElementById("submitCityButton");
+submitCityButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const cityInputText = document.getElementById("cityInputText").value;
+  fetchCoordinates(cityInputText).then((coords) => {
+    fetchWeather(coords.lat, coords.lon).then((weather) =>
+      console.log(weather)
+    );
+  });
+});
+
+// initApp();
