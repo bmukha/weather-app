3211ffcd68d3f32db3b1a5fca0255edcad6e9f8c
Bohdan <by.muha@gmail.com>
2021-04-26 18:51:54 +0300

    subminCityButton refactored to async\await

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,17 @@
 import renderWeather from "./dom";
 import { fetchWeather, fetchCoordinates } from "./fetch";
 
-const initApp = () => {
-  renderWeather(fetchWeather());
-};
+// const initApp = () => {
+//   renderWeather(fetchWeather());
+// };
 
 const submitCityButton = document.getElementById("submitCityButton");
-submitCityButton.addEventListener("click", (e) => {
+submitCityButton.addEventListener("click", async (e) => {
   e.preventDefault();
   const cityInputText = document.getElementById("cityInputText").value;
-  fetchCoordinates(cityInputText).then((coords) => {
-    fetchWeather(coords.lat, coords.lon).then((weather) =>
-      console.log(weather)
-    );
-  });
+  const coordinates = await fetchCoordinates(cityInputText);
+  const weather = await fetchWeather(coordinates.lat, coordinates.lon);
+  console.log(weather);
 });
 
 // initApp();
