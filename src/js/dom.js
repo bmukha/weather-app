2b46ba415180a5ae76b75ea2856e2ae940834248
Bohdan <by.muha@gmail.com>
2021-04-28 19:19:13 +0300

    daily weather styling started

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,14 +1,40 @@
+const DailyWeather = (
+  temperature,
+  humidity,
+  pressure,
+  pic
+) => `<div class="daily-wrapper">
+  <div class='temperature'>${temperature}℃</div>
+  <div class='humidity'>${humidity}</div>
+  <div class='pressure'>${pressure}</div>
+  <img src='http://openweathermap.org/img/wn/${pic}.png' id='current-pic'><img>
+</div> 
+`;
+
 const renderWeather = async (weatherData) => {
   const info = await weatherData;
   const currentWeatherInfo = info.current;
   const currentWeatherWrapper = document.getElementById("current-weather");
-  const currentWeatherPic = document.createElement("img");
-  const {temp, humidity, pressure} = currentWeatherInfo;
+  const { temp, humidity, pressure } = currentWeatherInfo;
   const currentWeatherContents = `<div id='current-temp'>${temp}℃</div>
   <div id='current-humidity'>${humidity}</div>
   <div id='current-pressure'>${pressure}</div>
-  <img src='http://openweathermap.org/img/wn/${currentWeatherInfo.weather[0].icon}@2x.png' id='current-pic'><img>`
+  <img src='http://openweathermap.org/img/wn/${currentWeatherInfo.weather[0].icon}@2x.png' id='current-pic'><img>`;
   currentWeatherWrapper.innerHTML = currentWeatherContents;
+
+  const dailyWeatherWrapper = document.getElementById("daily-weather");
+  let dailyWeatherContents = "";
+  const dailyWeatherInfo = info.daily;
+  for (let i = 0; i < dailyWeatherInfo.length; i++) {
+    dailyWeatherContents += DailyWeather(
+      dailyWeatherInfo[i].temp.day,
+      dailyWeatherInfo[i].humidity,
+      dailyWeatherInfo[i].pressure,
+      dailyWeatherInfo[i].weather[0].icon
+    );
+  }
+
+  dailyWeatherWrapper.innerHTML = dailyWeatherContents;
 };
 
 export default renderWeather;
